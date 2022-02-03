de3d2a40a26434ffe8edb6faebcd0ee63a7d20ab
jpeg25 <jairo25509@hotmail.com>
2022-02-02 20:34:07 -0500

    Initial commit.

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+// index.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+function App() {
+  return <h1>Your super duper React Playground is ready!</h1>;
+}
+ReactDOM.render(<App />, document.getElementById("root"));
